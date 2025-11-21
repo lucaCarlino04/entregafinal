8cdd3741b4b5d0998a17aaca423cc12da06847b8
lucaCarlino04 <lucacarlinounahur@gmail.com>
2025-11-21 20:15:19 -0300

    Creado el molde para el backend. La BDD se llama techlabdb

---
+++ src/main/java/com/techlab/entregafinal/EntregafinalApplication.java
@@ -0,0 +1,13 @@
+package com.techlab.entregafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregafinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregafinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techlab/entregafinal/EntregafinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.entregafinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregafinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
